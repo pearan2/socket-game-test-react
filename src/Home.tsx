1f5fbb71c7296fce0c2495b5a64d4933641f6fea
Hongkwon Lee <honlee@c4r4s8.42seoul.kr>
2021-07-08 22:59:19 +0900

    헤더기능추가

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Layout, Avatar, Menu, Dropdown } from "antd";
 import { Route, Switch } from "react-router-dom";
 import ChessLobby from "./ChessLobby";
 import ChessRoom from "./ChessRoom";
@@ -6,10 +6,45 @@ import ChessRoom from "./ChessRoom";
 const { Header, Footer, Content } = Layout;
 
 const Home = () => {
+  const menu = (
+    <Menu>
+      <Menu.Item>1st menu item</Menu.Item>
+      <Menu.Item>2st menu item</Menu.Item>
+      <Menu.Item>3st menu item</Menu.Item>
+    </Menu>
+  );
+
   return (
     <>
       <Layout className="HomeLayout">
-        <Header className="HomeHeader">Header</Header>
+        <Header
+          className="HomeHeader"
+          style={{
+            display: "flex",
+            justifyContent: "space-between"
+          }}
+        >
+          <div>{`5인 미만 사업장`}</div>
+
+          <Dropdown overlay={menu}>
+            <a style={{ color: "black" }} onClick={e => e.preventDefault()}>
+              <div
+                onClick={() => {
+                  console.log(`clicked!`);
+                }}
+                style={{ display: "flex", cursor: "pointer" }}
+              >
+                <div>{`Honlee`}</div>
+                <div>
+                  <Avatar
+                    size="large"
+                    src={`https://cdn.intra.42.fr/users/small_honlee.jpg`}
+                  ></Avatar>
+                </div>
+              </div>
+            </a>
+          </Dropdown>
+        </Header>
         <Content>
           <Switch>
             <Route exact path="/" component={ChessLobby} />
